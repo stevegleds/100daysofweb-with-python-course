6da739bc4c24da54e9327a744701db8984751f39
stevegleds <stevegleds@gmail.com>
2019-05-04 07:11:54 +0100

    layout changes

--- days/001-004-flask-intro/code/program/routes.py
+++ days/001-004-flask-intro/code/program/routes.py
@@ -1,10 +1,12 @@
 from flask import render_template
 from program import app
 
+
 @app.route('/')
 @app.route('/index')
 def index():
     return render_template('index.html', title='Template Demo', time=timenow)
+
 
 @app.route('/100Days')
 def p100days():
