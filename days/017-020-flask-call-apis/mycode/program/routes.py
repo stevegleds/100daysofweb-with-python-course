2de5b5421601af16789cb68eff4ecf0ff3c223cf
Steve Gledhill <stevegleds@gmail.com>
2019-05-20 10:43:38 +0100

    20 May 2019 Back on PC and 'no flask' error. Simply installed Flask package (opened this 'day' in new window and installed package). All OK Created Chuck Norris joke api page. Next steps: Add menu to link to page Find new api from https://medium.com/@vicbergquist/18-fun-apis-for-your-next-project-8008841c7be9 Implement

--- days/017-020-flask-call-apis/mycode/program/routes.py
+++ days/017-020-flask-call-apis/mycode/program/routes.py
@@ -1,8 +1,10 @@
 from flask import render_template
 from program import app
 from datetime import datetime
+import requests
 
 timenow = str(datetime.today())
+
 
 @app.route('/')
 @app.route('/index')
@@ -13,3 +15,15 @@ def index():
 @app.route('/100Days')
 def p100days():
     return render_template('100Days.html')
+
+
+@app.route('/chuck')
+def chuck():
+    joke = get_chuck_joke()
+    return render_template('chuck.html', joke=joke)
+
+
+def get_chuck_joke():
+    r = requests.get('https://api.chucknorris.io/jokes/random')
+    data = r.json()
+    return data['value']
