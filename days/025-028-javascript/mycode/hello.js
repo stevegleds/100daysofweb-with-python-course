53036952b8eb1bf3f066832e2c1ed62dd170595d
Steve Gledhill <stevegleds@gmail.com>
2019-06-21 20:26:42 +0100

    21 June 2019 VS Code is working out OK. Installed some plugins to help. Completed day one of JavaScript (day 25). Next is day 2 which is the exercises. I created folders in numerical order to try out what I learned.

--- days/025-028-javascript/mycode/hello.js
+++ days/025-028-javascript/mycode/hello.js
@@ -2,4 +2,31 @@ function hello(name){
 	return 'Hello ' + name;
 }
 
-console.log(hello('bob'));
+console.log(hello());
+
+function toCelcius(fahrenheit){
+	return (5/9) * (fahrenheit - 32);
+}
+
+function print_arguments(){
+	for(let arg of arguments){
+		console.log(arg);
+	}
+}
+// default parameter
+function hello(name){
+	if(name === undefined) name = 'stranger'
+	console.log('Hello ' + name);
+}
+
+// new way
+function hello(name='stranger'){
+	console.log(`Hello ${name}`)
+}
+
+// a shorter way
+let hello_arrow = (name='stranger') => 'Hello ' + name
+
+// Objects (like dictionaries in python. this is equivalent to self)
+let bite = {'number': 1, 'title': 'sum of numbers', 'points': 2}
+bite.str = function(){console.log(`Bite : ${this.number} - ${this.title} - points ${this.points}`)}
